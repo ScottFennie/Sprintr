7057ca3b3966fca44249711c7c1a271c1c2c96ea
nathan <nateohendricks@gmail.com>
2021-09-27 15:54:26 -0600

    sync

--- Sprintr.client/src/env.js
+++ Sprintr.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'scottsauth.us.auth0.com'
+export const clientId = 'xybeA568NKruZQZme7p9HQ9y8X4DB4po'
+export const audience = 'https://studentauth.com'
