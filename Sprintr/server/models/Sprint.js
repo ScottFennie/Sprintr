9b8ee4ebac695787cf01d70bf0aa495b58a4f2fd
Scott Fennie <s.fennie7@gmail.com>
2021-09-27 16:57:57 -0600

    sprints need controller/services

---
+++ Sprintr/server/models/Sprint.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+import { ProjectSchema } from './Project'
+const Schema = mongoose.Schema
+
+export const SprintSchema = new Schema({
+  name: { type: String, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  projectId: { type: Schema.Types.ObjectId, required: true, ref: 'Project' },
+  creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' }
+
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+SprintSchema.virtual('projects', {
+  localField: 'projectId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Project'
+})
+SprintSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  ref: 'Account',
+  justOne: true
+})
