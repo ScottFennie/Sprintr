9a73e79a0ec92e827e87645b3cebb56e853e6dbf
Josh <joshuarmorrison@gmail.com>
2021-09-28 14:03:44 -0600

    Task service complete

--- Sprintr/server/controllers/TaskController.js
+++ Sprintr/server/controllers/TaskController.js
@@ -1,1 +1,52 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { taskService } from '../services/TaskService'
+import BaseController from '../utils/BaseController'
 
+export class TaskController extends BaseController {
+  constructor() {
+    super('api/projects/:projectId/backlog/:backlogId/task')
+    this.router
+      .get('', this.getTasks)
+      .get('/:taskId', this.getTaskById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createTask)
+      .delete('/:taskId', this.deleteTask)
+  }
+
+  async getTaskById(req, res, next) {
+    try {
+      const task = await taskService.getTaskById(req.params.taskId)
+      res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getTasks(req, res, next) {
+    try {
+      const tasks = await taskService.getTasks(req.params.projectId)
+      res.send(tasks)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createTask(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const task = await taskService.createTask(req.body)
+      res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteTask(req, res, next) {
+    try {
+      const task = await taskService.deleteTask(req.params.sprintId, req.userInfo.id)
+      res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
