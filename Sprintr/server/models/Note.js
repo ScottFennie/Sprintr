1099ccf1c8d3c7e7506160a10bfa16e1a5592c59
nathan <nateohendricks@gmail.com>
2021-09-28 12:43:18 -0600

    Notes and Task schemas

---
+++ Sprintr/server/models/Note.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export const NoteSchema = new Schema({
+  body: { type: String, required: true },
+  backlogItemId: { type: Schema.Types.ObjectId, required: true, ref: 'BacklogItem' },
+  projectId: { type: Schema.Types.ObjectId, required: true, ref: 'Project' },
+  creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' }
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+NoteSchema.virtual('backlogitem', {
+  localField: 'backlogItemId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'BacklogItem'
+})
+NoteSchema.virtual('projects', {
+  localField: 'projectId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Project'
+})
+NoteSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  ref: 'Account',
+  justOne: true
+})
---
+++ Sprintr/server/models/Task.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export const TaskSchema = new Schema({
+  name: { type: String, required: true },
+  weight: { type: Number, required: true },
+  backlogItemId: { type: Schema.Types.ObjectId, required: true, ref: 'BacklogItem' },
+  projectId: { type: Schema.Types.ObjectId, required: true, ref: 'Project' },
+  creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' }
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+TaskSchema.virtual('backlogitem', {
+  localField: 'backlogItemId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'BacklogItem'
+})
+TaskSchema.virtual('projects', {
+  localField: 'projectId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Project'
+})
+TaskSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  ref: 'Account',
+  justOne: true
+})
