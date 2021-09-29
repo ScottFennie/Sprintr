71c346c8e9dac0de1b7f7fee9049321d65741d68
nathan <nateohendricks@gmail.com>
2021-09-29 10:58:08 -0600

    about to backlog

--- Sprintr.client/src/router.js
+++ Sprintr.client/src/router.js
@@ -12,9 +12,9 @@ const routes = [
     component: loadPage('HomePage')
   },
   {
-    path: '/about',
-    name: 'About',
-    component: loadPage('AboutPage')
+    path: '/backlog',
+    name: 'Backlog',
+    component: loadPage('BacklogPage')
   },
   {
     path: '/account',
