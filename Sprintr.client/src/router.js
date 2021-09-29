c38b2d404a37924fa6ef5a3ed75a6719172bc4c9
Scott Fennie <s.fennie7@gmail.com>
2021-09-29 11:03:18 -0600

    projects page

--- Sprintr.client/src/router.js
+++ Sprintr.client/src/router.js
@@ -8,8 +8,8 @@ function loadPage(page) {
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: loadPage('HomePage')
+    name: 'Projects',
+    component: loadPage('ProjectsPage')
   },
   {
     path: '/about',
