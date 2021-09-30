ebf64b0ad0aba932c4b4d127c4b7c90be7cc4e4a
Josh <joshuarmorrison@gmail.com>
2021-09-30 11:55:34 -0600

    task form

---
+++ Sprintr.client/src/components/TaskForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-fluid mb-3">
+    <div class="row">
+      <div class="col-lg-10">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+      </div>
+      <div class="col-lg-2">
+        <div class="mb-3"></div>
+        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
